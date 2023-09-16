44c07955f8e46485cd008e94c63e5889e670f308
YeKaTa <355833@naver.com>
2023-09-16 21:19:57 +0900

    기타 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,9 @@ export default defineNuxtConfig({
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=Rowdies:wght@300&family=Ubuntu&display=swap'
       }]
-    }
+    },
+    baseURL: '',
+    buildAssetsDir: 'assets'
   },
   devServer: {
     port: process.env.NODE_ENV === 'production' ? undefined : 5000
